167b04fced7494cd512667fa92b67795a3e22ee5
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-06 22:52:12 +0200

    python done with Kadane

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,20 @@
 def picker(prices):
-    pass
+
+    # Kadane's works here, we initialize the max profit on -inf
+    
+    max_profit = float("-inf")
+    curr = 0
+    coordinates = []
+
+    # we get all coordinates where j is after i to avoid the sell before buy
+
+    for i in range(len(prices)):
+        for j in range(i,len(prices)):
+            profit = prices[j] - prices[i]
+            # if the current profit is better than max, we update the max and replace the coordinates
+            curr = max (profit,curr)
+            if curr > max_profit:
+                max_profit = max(max_profit,curr)
+                coordinates = [i,j]
+    return coordinates
+
