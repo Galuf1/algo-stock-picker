9dd6d3b42c87588c1e1fea8769be6316d2e2fc8b
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-07 12:17:43 +0200

    solved

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,19 @@
+
 exports.picker = function(prices) {
 
+    let max_profit = -Infinity
+    let curr = 0
+    let coordinates = []
+
+    for (let i = 0; i < prices.length; i++){
+        for (let j = i; j < prices.length; j++){
+            profit = prices[j] - prices[i]
+            curr = Math.max(profit,curr)
+            if (curr > max_profit){
+                max_profit = Math.max(max_profit,curr)
+                coordinates = [i,j]
+            }
+        }
+    }
+    return coordinates
 }
